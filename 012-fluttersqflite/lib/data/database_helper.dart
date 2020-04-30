ad0e01b9a19af6bb4365519ea06f9c8c1f951ef1
Eric Omine <5587665-ericomine@users.noreply.gitlab.com>
2020-04-30 18:40:03 -0300

    Implementing database_helper.dart

---
+++ 012-fluttersqflite/lib/data/database_helper.dart
@@ -0,0 +1,48 @@
+import 'package:fluttersqflite/models/user.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+import 'dart:io' as io;
+
+class DatabaseHelper {
+  // Implementing as a singleton
+  static final _instance = DatabaseHelper.internal();
+  DatabaseHelper.internal();
+  factory DatabaseHelper() => _instance;
+
+  static Database _db;
+  
+  Future<Database> get db async {
+    if (_db != null) {
+      return _db;
+    }
+
+    _db = await initDb();
+    return _db;
+  }
+  
+  initDb() async {
+    io.Directory documentDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentDirectory.path, "main.db");
+    var myDb = await openDatabase(path, version: 1, onCreate: _onCreateDb);
+    return myDb;
+  }
+
+  void _onCreate(Database db, int version) async {
+    await db.execute("""
+      CREATE TABLE User(id INTEGER PRIMARY KEY, username TEXT, password TEXT)
+    """);
+    print("Table User is created");
+  }
+
+  Future<int> saveUser(User user) async {
+    var dbClient = await db;
+    int res = await dbClient.insert("User", user.toMap());
+    return res;
+  }
+
+  Future<int> deleteUser(User user) async {
+    var dbClient = await db;
+    int res = await dbClient.delete("User");
+    return res;
+  }
+}
