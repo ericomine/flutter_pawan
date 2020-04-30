1be70b6355ccd875064eef42f35f4b763070b6fa
Eric Omine <5587665-ericomine@users.noreply.gitlab.com>
2020-04-30 00:22:40 -0300

    Deserializing Json from Http Response

--- 005-fluttercrypto/lib/main.dart
+++ 005-fluttercrypto/lib/main.dart
@@ -1,111 +1,88 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(
+      home: HomePage(),
+    ));
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class HomePage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List currencies;
+  final List<MaterialColor> _colors = [
+    Colors.blue,
+    Colors.indigo,
+    Colors.red,
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrencies();
+  }
+
+  Future<String> getCurrencies() async {
+    String cryptoUrl = 'pro-api.coinmarketcap.com';
+    String endpoint = '/v1/cryptocurrency/listings/latest';
+    var queryParams = {'start': '1', 'limit': '50', 'convert': 'USD'};
+    var uri = Uri.https(cryptoUrl, endpoint, queryParams);
+    var response = await http.get(
+      uri,
+      headers: {
+        'Accept': 'application/json',
+        'X-CMC_PRO_API_KEY': 'b9a3c6ff-804d-4a43-b29e-162ca73ef380',
+      },
     );
-  }
-}
+    var data = jsonDecode(response.body);
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+    if (data['status']['error_code'] != 0) {
+      return 'Error';
+    }
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      currencies = data['data'];
     });
+    return 'Success';
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("CryptoApp"),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: _cryptoWidget(),
     );
   }
+
+  Widget _cryptoWidget() {
+    return Container(
+      child: ListView.builder(
+        itemCount: currencies == null ? 0 : currencies.length,
+        itemBuilder: (BuildContext context, int index) {
+          final Map currency = currencies[index];
+          final MaterialColor color = _colors[index % _colors.length];
+          return _getListItemUI(currency, color);
+        },
+      ),
+    );
+  }
+
+  ListTile _getListItemUI(Map currency, MaterialColor color) {
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundColor: color,
+        child: Text(currency['name'][0]),
+      ),
+      title: Text(
+        currency['name'],
+        style: TextStyle(fontWeight: FontWeight.bold),
+      ),
+    );
+  }  
 }
